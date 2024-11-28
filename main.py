ca8424e0cc0275922df1ef2af67db657807c6d45
bibishop <bolotovakadica455@gmail.com>
2024-11-29 05:31:59 +0600

    covering the indexes
    In this step I have done very big changes ,I have converted the Letter to the index .It seems that further it will be much more complex

--- main.py
+++ main.py
@@ -1,69 +1,84 @@
 def get_shot(guesses):
-    ok = "n"
-    while ok == "n":
+    """Prompt the user for a shot and return the shot as a (column, row) tuple."""
+    while True:
         try:
-            shot = input("Please enter your guess (0-48): ")
-            shot = int(shot)
-            if shot < 0 or shot > 48:
-                print("Invalid number, please try again.")
-            elif shot in guesses:
+            shot = input("Please enter your guess (e.g., B5): ").upper()
+            
+            if len(shot) != 2:
+                print("Invalid input! Please enter a letter followed by a number (e.g., B5).")
+                continue
+
+            column = shot[0]
+            row = int(shot[1])
+
+            if column not in "ABCDEFG" or row not in range(1, 8):
+                print("Invalid column or row. Please enter a valid guess (e.g., B5).")
+                continue
+
+            if (column, row) in guesses:
                 print("You've already guessed this spot!")
-            else:
-                ok = "Yes"
-                break
+                continue
+
+            return column, row  
         except ValueError:
-            print("Incorrect entry, please enter a number between 0 and 48:")
-    return shot
+            print("Incorrect entry, please enter a letter and a number (e.g., B5).")
 
 def show_board(hit, miss, comp):
+    """Display the board showing hits, misses, and remaining boats."""
     print("     Battleship  ")
-    print("   A B C D E F G")
+    print("   A B C D E F G")  
     
-    place = 0
-    for x in range(7):
-        row = ""
-        for i in range(7):
+    for row in range(1, 8):
+        row_display = ""
+        for column in "ABCDEFG":
             ch = "_ "
-            if place in miss:
-                ch = "x "
-            elif place in hit:
-                ch = "o "
-            elif place in comp:
-                ch = "O "
-            row = row + ch
-            place = place + 1
-        print(x, "", row)
-def check_shot(boat1,boat2,hit,miss,comp):
-    if shot in boat1:
-        
-        boat1.remove(shot)
-        if len(boat1)>0:
-            hit.append(shot)    
-        else :
-            comp.append(shot)
-   
-    elif shot in boat2:
-        boat1.remove(shot)
-        if len(boat2)>0:
-            hit.append(shot)    
-        else :
-            comp.append(shot)
-    else:
-        miss.append(shot)    return hit,miss,comp,boat1,boat2
-boat1=[45,46,47]
-boat2=[6,16,26]
-hit = []  # List of hit positions
-miss = []  # List of missed positions
-comp = [] 
+            if (column, row) in miss:
+                ch = "x " 
+            elif (column, row) in hit:
+                ch = "o "  
+            elif (column, row) in comp:
+                ch = "O "  
+            row_display += ch
+        print(f"{row}  {row_display}")  
 
-for i in range (10):
-    guesses = hit + miss + comp  # All positions that have been guessed
-    shot=get_shot(guesses)
-    boat1,boat2,hit,miss,comp=check_shot(boat2,boat1,hit,miss,comp)
-    show_board(hit, miss, comp)
-    if len(boat1)<1 and len(boat2)<1:
-        print("You have won")
+def check_shot(shot, boat1, boat2, hit, miss, comp):
+    """Check if the shot hits any boats and update the lists accordingly."""
+    if shot in boat1:
+        boat1.remove(shot)
+        hit.append(shot)
+        if len(boat1) == 0:
+            print("Boat 1 is sunk!")
+            comp.extend(hit)  
+    elif shot in boat2:
+        boat2.remove(shot)
+        hit.append(shot)
+        if len(boat2) == 0:
+            print("Boat 2 is sunk!")
+            comp.extend(hit)
+    else:
+        miss.append(shot)
+    return hit, miss, comp, boat1, boat2
+
+
+boat1 = [("B", 5), ("C", 5), ("D", 5)]  
+boat2 = [("A", 2), ("A", 3), ("A", 4)]  
+
+hit = []  
+miss = [] 
+comp = []  
+
+
+for i in range(10):
+    guesses = hit + miss + comp  
+    shot = get_shot(guesses)  
+    hit, miss, comp, boat1, boat2 = check_shot(shot, boat1, boat2, hit, miss, comp)  
+    show_board(hit, miss, comp)  
+
+ 
+    if len(boat1) == 0 and len(boat2) == 0:
+        print("You have won!")
         break
-print("Congratulation you have finished the game  ")
+
+print("Congratulations, you have finished the game!")
 
 
