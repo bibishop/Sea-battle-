c112771bb4179a3e8ac53e36e1f3763019f5b749
bibishop <bolotovakadica455@gmail.com>
2024-11-29 03:52:54 +0600

    Showing the board

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
 print("Welcome to Sea battle game ")
 m=input("What is your name")
 print("Welcome to the game"+m)
+def show_board(hit,miss,comp):
+    print("   Battleship  ")
+    print("   A B C D E F G")
+    place=0
+    for x in range(7):
+        row=""
+        for i in range(7):
+            ch="_ "
+            if place in miss :
+                ch="x "
+            elif place in hit :
+                ch="o "
+            elif place in comp:
+                ch="O "
+                
+            row=row+ch
+        print(x,"",row)
+print(show_board)
+hit=[21,22]
+miss=[20,21,12,13]
+comp=[23]
+
+
+show_board(hit,miss,comp)
